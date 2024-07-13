3d64ada6756c3d46c134c730e95f64d45f74ead4
laiba kanwal <kanwallaiba873@gmail.com>
2024-07-13 21:56:21 +0500

    Day1 challenge completed

---
+++ day1.js
@@ -0,0 +1,53 @@
+// Variables and Data Types
+
+// Activity 1
+// Task1
+var children = 10;
+console.log(children);
+
+// Task2
+let str = 'hello laiba';
+console.log(str);
+
+// Activity 2
+// Task3
+const isPaid = false;
+console.log(isPaid);
+
+// Activity 3
+// Task4
+let count = 5;
+let greet = 'Welcome back dear';
+let examPassed = true;
+let students = {
+    id: 1,
+    stdName: 'aba',
+    course: 'javascript'
+}
+const courses = ['java', 'css', 'html','javascript'];
+let stars;
+
+console.log(count,typeof(count));           //number
+console.log(greet,typeof(greet));           //string
+console.log(examPassed,typeof(examPassed)); //boolean
+console.log(students,typeof(students));     //object
+console.log(courses,typeof(courses));       //object
+console.log(stars,typeof(stars));           //undefined
+console.log(null,typeof(null));             //object
+
+// Activity 4
+// Task5
+
+let game = 'cricket';
+console.log(game);
+
+game = 'football';
+console.log(game);
+
+// Activity 5
+// Task6
+
+const months = 12;
+console.log(months);
+
+// months = 13; //Trying to reassign a const variable will throw an error
